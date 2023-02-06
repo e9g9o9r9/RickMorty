76f7ab2830ec6ea37d2618a57dff104fd386d1e7
Egor Romanchuk <romanchuk.fortech@gmail.com>
2023-02-06 15:18:53 +0300

    fix: fix refresh problem gh-pages

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const client = new ApolloClient({
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/RickMorty">
     <ApolloProvider client={client}>
       <Global />
       <App />
